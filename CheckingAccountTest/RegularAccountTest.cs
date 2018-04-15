e9216cb8e43f34c745cfa959deb54cc6769769c8
Andrew Hinson <hinsonan@gmail.com>
2018-04-14 20:09:17 -0500

    initial commit

---
+++ CheckingAccountTest/RegularAccountTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CheckingAccountTest
+{
+    public class RegularAccountTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
